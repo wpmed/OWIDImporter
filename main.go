5494cf016214b4d1779fa7ec61196e213e9a98d3
Hassan Amin <hassanmamin994@gmail.com>
2026-03-16 07:03:20 +0200

    reduce concurrency to 1 task

--- main.go
+++ main.go
@@ -71,7 +71,7 @@ func monitorQueuedTasks() {
 			continue
 		}
 
-		if count < 2 {
+		if count < 1 {
 			task, err := models.FindNextTaskToProcess()
 			if err != nil {
 				fmt.Println("Error finding next task to process", err)
