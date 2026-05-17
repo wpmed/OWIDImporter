7210ab78dc223550b5f1e03d6141bcf697b8051f
Hassan Amin <hassanmamin994@gmail.com>
2026-05-17 20:16:39 +0300

    update MAP_TOOLTIP_SELECTOR selector

--- services/selectors.go
+++ services/selectors.go
@@ -12,7 +12,7 @@ const (
 	COUNTRY_SELECTED_OPTIONS_LIST   = ".entity-selector__content .entity-section ul li[data-flip-id^=\"selected_\"], .EntityList label.EntityPickerOption.selected .name"
 	COUNTRY_SEARCH_INPUT            = ".entity-selector__search-bar input, .EntityPicker .EntityPickerSearchInput input"
 	COUNTRY_SEARCH_RESULT_LIST      = ".entity-selector__content ul li label, .EntityPicker .EntityList label.EntityPickerOption .name"
-	MAP_TOOLTIP_SELECTOR            = "#mapTooltip svg"
+	MAP_TOOLTIP_SELECTOR            = "#mapTooltip .content svg"
 	TABS_LABELS_SELECTOR            = ".ContentSwitchers__Container div.Tabs div.Tabs__Tab .label"
 	TABS_LABELS_ACTIVE_TAB_SELECTOR = ".ContentSwitchers__Container div.Tabs div.Tabs__Tab.active"
 )
